1aa5d3e7974ffda986ca13e5b797ad1d3db83655
Dylan Frankland <dfrankland@users.noreply.github.com>
2022-03-16 21:58:59 -0700

    Add trunk package

---
+++ pkgs/trunk/default.nix
@@ -0,0 +1,36 @@
+{ autoPatchelfHook, fetchurl, makeWrapper, lib, stdenv }:
+
+with lib;
+
+let
+  # get the lastest version from https://trunk.io/releases/latest
+  version = "0.8.1-beta";
+  platform = if (stdenv.isDarwin) then "darwin" else "linux";
+  sha256 = if (stdenv.isDarwin) then "sha256-sjB4dCUYFNbLwXtOWq0O8yRsPNxPk5w7pt6FWJpsXg4=" else "6f4ce9ea0d0f249536fda82f94304e1642cf0637709acba6cdab26ca36253140";
+in
+stdenv.mkDerivation {
+  pname = "trunk";
+  inherit version;
+  src = fetchurl {
+    url = "https://trunk.io/releases/${version}/trunk-${version}.${platform}.tar.gz";
+    inherit sha256;
+  };
+
+  # Work around the "unpacker appears to have produced no directories"
+  # case that happens when the archive doesn't have a subdirectory.
+  setSourceRoot = "sourceRoot=`pwd`";
+
+  nativeBuildInputs = [ autoPatchelfHook makeWrapper ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ./trunk $out/bin/
+  '';
+
+  meta = with lib; {
+    description = "Trunk all-in-one tool for scalably checking, testing, merging, and monitoring code";
+    homepage = "https://trunk.io";
+    license = licenses.unfree;
+    platforms = [ "x86_64-linux" "x86_64-darwin" ];
+  };
+}
