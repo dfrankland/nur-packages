d910a696f29b776f2f037f8cc0a9c72ad5ffaeff
Dylan Frankland <dfrankland@users.noreply.github.com>
2022-05-03 11:53:45 -0700

    update trunk

--- pkgs/trunk/default.nix
+++ pkgs/trunk/default.nix
@@ -4,15 +4,20 @@ with lib;
 
 let
   # get the lastest version from https://trunk.io/releases/latest
-  version = "0.10.0-beta";
+  version = "0.11.0-beta";
   platform = if (stdenv.isDarwin) then "darwin" else "linux";
-  sha256 = if (stdenv.isDarwin) then "37e186fce102c213564dff4d49389ae1306fdbfee3c28180ed9cce4a24061302" else "2a2177605b743b94aa36e5ea7fa100105ac5b3bc8132d8661e441396baa59c60";
+  cpu = if (stdenv.hostPlatform.isAarch64) then "arm64" else "x86_64";
+  sha256 = if (platform == "darwin" && cpu == "arm64")
+    then "c6b2ab71aff04ae510ed52faf92816a4d7f1559b2647e4de16cf7e88c5ac5afb"
+    else if (platform == "darwin" && cpu == "x86_64")
+    then "2a2177605b743b94aa36e5ea7fa100105ac5b3bc8132d8661e441396baa59c60"
+    else "791979441af605c631338ede3f442cc3c7d27e262b95777faf0c965de6e19ea4";
 in
 stdenv.mkDerivation {
   pname = "trunk";
   inherit version;
   src = fetchurl {
-    url = "https://trunk.io/releases/${version}/trunk-${version}.${platform}.tar.gz";
+    url = "https://trunk.io/releases/${version}/trunk-${version}-${platform}-${cpu}.tar.gz";
     inherit sha256;
   };
 
