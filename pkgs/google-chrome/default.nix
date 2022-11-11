ba2f0c6b5b6b22f9c29d2db8839720cc43a64548
Dylan Frankland <dfrankland@users.noreply.github.com>
2022-11-11 15:53:23 -0800

    update google-chrome sha

--- pkgs/google-chrome/default.nix
+++ pkgs/google-chrome/default.nix
@@ -8,11 +8,11 @@ else
   in
   stdenv.mkDerivation {
     pname = "google-chrome";
-    version = "106.0.5249.119";
+    version = "107.0.5304.110";
 
     src = fetchurl {
       url = "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg";
-      sha256 = "sha256-o5MyHPxWh5gDm6iZB7yJwn+6R61rlMj9qKrTPvn0RM0=";
+      sha256 = "sha256-IBc508+UHTPa9gU1EWDyK91YdwcCZ+K0LzfvpmE3h3I=";
     };
 
     sourceRoot = app;
