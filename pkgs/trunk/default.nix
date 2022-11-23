fca33200963807ef3ad0aaaa9852737671667daa
Dylan Frankland <dfrankland@users.noreply.github.com>
2022-11-23 11:33:55 -0800

    update trunk

--- pkgs/trunk/default.nix
+++ pkgs/trunk/default.nix
@@ -4,15 +4,15 @@ with lib;
 
 let
   # get the lastest version from https://trunk.io/releases/latest
-  version = "0.18.1-beta";
+  version = "1.1.0";
   platform = if (stdenv.isDarwin) then "darwin" else "linux";
   cpu = if (stdenv.hostPlatform.isAarch64) then "arm64" else "x86_64";
   sha256 =
     if (platform == "darwin" && cpu == "arm64")
-    then "c25df7ef6d68a2bd8648d93830659d1675043db03d9f49182534f28e417dfda6"
+    then "d3ebd10e99ef1508f71787a2bc2a39099121ac1391b1e03971d932f78b87bb6b"
     else if (platform == "darwin" && cpu == "x86_64")
-    then "6a7a4f9c5d0ad7d3d5ea45e6a1cf11a668ea68c36f01ebe31ef95f99560a6824"
-    else "d875fe70c221d6449a9cd09598ab278133c5513f52dbf6af0fca3161d7a39d46";
+    then "4a1d71e39db4948a382f891d69a4e32675a811c5f25d06328c4922abac377250"
+    else "b08c7fe2f2b91151873c43590896ef0433dd7a219f76805ce421d6fe7b1643d4";
 in
 stdenv.mkDerivation {
   pname = "trunk";
