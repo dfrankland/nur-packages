dd9f726f59da4d9487b8f107080d0d840f923756
Dylan Frankland <dfrankland@users.noreply.github.com>
2023-10-10 20:13:18 -0700

    update google-chrome

--- default.nix
+++ default.nix
@@ -14,7 +14,7 @@ in
   docker-desktop = pkgs.callPackage ./pkgs/docker-desktop { };
   gbdk-2020 = pkgs.callPackage ./pkgs/gbdk-2020 { };
   github-desktop = pkgs.callPackage ./pkgs/github-desktop { };
-  google-chrome = pkgs.callPackage ./pkgs/google-chrome { };
+  google-chrome = pkgs.callPackage ./pkgs/google-chrome { inherit unpackdmg; };
   lcov = pkgs.callPackage ./pkgs/lcov { };
   qmk_toolbox = pkgs.callPackage ./pkgs/qmk_toolbox { };
   rectangle = pkgs.callPackage ./pkgs/rectangle { };
--- pkgs/google-chrome/default.nix
+++ pkgs/google-chrome/default.nix
@@ -1,26 +1,21 @@
-{ lib, stdenv, fetchurl, undmg, google-chrome }:
+{ lib, stdenv, fetchurl, unpackdmg, google-chrome }:
 
 if (!stdenv.isDarwin) then
   google-chrome
 else
-  let
-    app = "Google Chrome.app";
-  in
   stdenv.mkDerivation {
     pname = "google-chrome";
-    version = "115.0.5790.170";
+    version = "118.0.5993.70";
 
     src = fetchurl {
       url = "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg";
-      sha256 = "sha256-C59+BqvWtsAH0Xgpz4/9e+3Ljhhfu6igOA1pGejrUCk=";
+      sha256 = "sha256-6zIFAT27XJ9rfKCI89iaPXhIiFew8SGhX5WibOk6Yj8=";
     };
 
-    sourceRoot = app;
-
-    buildInputs = [ undmg ];
+    buildInputs = [ unpackdmg ];
     installPhase = ''
-      mkdir -p "$out/Applications/${app}"
-      cp -R . "$out/Applications/${app}"
+      mkdir -p "$out/Applications"
+      cp -R 'Google Chrome.app' "$out/Applications/"
     '';
 
     meta = {
