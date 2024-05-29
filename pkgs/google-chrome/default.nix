c9c06252d39a5a52a07971f2bc0a0c1c26f9afd2
Dylan Frankland <dfrankland@users.noreply.github.com>
2024-05-28 19:38:11 -0700

    upgrade google-chrome

--- pkgs/google-chrome/default.nix
+++ pkgs/google-chrome/default.nix
@@ -6,11 +6,11 @@ else
 # https://formulae.brew.sh/api/cask/google-chrome.json
   stdenv.mkDerivation {
     pname = "google-chrome";
-    version = "124.0.6367.208";
+    version = "125.0.6422.113";
 
     src = fetchurl {
       url = "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg";
-      sha256 = "sha256-F8AkkyqHSr+tJKns2pG3ZCUTUug5KLZALxEUHHUKVkc=";
+      sha256 = "sha256-fWjEha1McWDT13c7eESMaUmKy2G22yBeAD92JO7b4uA=";
     };
 
     buildInputs = [ unpackdmg ];
