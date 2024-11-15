ccc2d223ec303441ae07096ea3ffdced0cadf7f4
Dylan Frankland <dfrankland@users.noreply.github.com>
2024-11-15 09:52:55 -0800

    update wavebox

--- pkgs/wavebox/default.nix
+++ pkgs/wavebox/default.nix
@@ -6,7 +6,7 @@ else
 # https://formulae.brew.sh/api/cask/wavebox.json
   let
     app = "Wavebox.app";
-    version = "10.127.16.2";
+    version = "10.130.3.2";
   in
   stdenv.mkDerivation {
     pname = "wavebox";
@@ -15,7 +15,7 @@ else
     src = fetchurl {
       name = "wavebox-${version}.dmg";
       url = "https://download.wavebox.app/stable/macuniversal/Install%20Wavebox%20${version}.dmg";
-      sha256 = "sha256-8QDRrh4ZtUrwfwWTQeYzGf5/zC9BEVgh1hvftyPtO0k=";
+      sha256 = "sha256-h5T7T0e9RDlxMalis9vrEnAcUm6ADRp+mtmGrYpljhU=";
     };
 
     sourceRoot = app;
