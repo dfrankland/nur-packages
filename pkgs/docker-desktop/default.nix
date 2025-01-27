74d930f270fa41543f235ee98de8387e8500baaa
Dylan Frankland <dfrankland@users.noreply.github.com>
2025-01-27 15:28:40 -0800

    update docker desktop

--- pkgs/docker-desktop/default.nix
+++ pkgs/docker-desktop/default.nix
@@ -2,16 +2,17 @@
 
 let
   # get the lastest version from https://docs.docker.com/desktop/release-notes/
-  version = "4.33.0";
-  rev = "160616";
+  version = "4.37.2";
+  rev = "179585";
   platform = if (stdenv.isDarwin) then "mac" else "linux";
   cpu = if (stdenv.hostPlatform.isAarch64) then "arm64" else "amd64";
   sha256 =
     if (platform == "mac" && cpu == "arm64")
-    then "33070a04c96d8778fd7aaa6f06b4b656b6d9cad243f6db7111b4aa560f6dedc4"
+    then "624dec2ae9fc2269e07533921f5905c53514d698858dde25ab10f28f80e333c7"
     else if (platform == "mac" && cpu == "amd64")
-    then "33070a04c96d8778fd7aaa6f06b4b656b6d9cad243f6db7111b4aa560f6dedc4"
-    else "087301fb44e421c1113389994ab5b4e50d25c9b3a0413460fa8abe1527a253fa";
+    then "5f58f3acff80bfadaef62e5873810f609ac77df6ebbde99d44360fb6aa93a45d"
+    # There's no `4.37.2` version for linux ¯\_(ツ)_/¯
+    else "";
   file = if (stdenv.isDarwin) then "Docker.dmg" else "docker-desktop-${version}-${cpu}.deb";
 in
 stdenv.mkDerivation {
